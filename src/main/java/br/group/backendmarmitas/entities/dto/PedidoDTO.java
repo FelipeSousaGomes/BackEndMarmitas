3081c2c65f8c797fe13a0d02ee09f079d46af9e0
Felipe <felipesouzagomes21@gmail.com>
2024-11-29 01:16:33 -0300

    create PedidoDTO

--- src/main/java/br/group/backendmarmitas/entities/dto/PedidoDTO.java
+++ src/main/java/br/group/backendmarmitas/entities/dto/PedidoDTO.java
@@ -1,14 +1,42 @@
 package br.group.backendmarmitas.entities.dto;
 
-import br.group.backendmarmitas.entities.Entregador;
-import br.group.backendmarmitas.entities.StatusDoPedido;
-import br.group.backendmarmitas.entities.User;
+import br.group.backendmarmitas.entities.*;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PedidoDTO {
     private Long id;
     private Instant data;
     private StatusDoPedido status;
+    private UserDTO user;
+    private EntregadorDTO entregador;
+
+    List<ItemPedidoDTO> ItensPedido = new ArrayList<>();
+
+  public PedidoDTO() {}
+
+
+    public PedidoDTO(Pedido pedido) {
+      this.id = pedido.getId();
+      this.data = pedido.getData();
+      this.status = pedido.getStatus();
+      this.user = new UserDTO(pedido.getUser());
+      this.entregador = new EntregadorDTO(pedido.getEntregador());
+        for (ItemPedido item: pedido.getItens()){
+            ItensPedido.add(new ItemPedidoDTO(item));
+        }
+    }
+
+
+    public Double getTotal(){
+        double sum = 0.0;
+        for (ItemPedidoDTO item : ItensPedido) {
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+
 }
--- src/main/java/br/group/backendmarmitas/services/AuthorizationService.java
+++ src/main/java/br/group/backendmarmitas/services/AuthorizationService.java
@@ -3,6 +3,9 @@ package br.group.backendmarmitas.services;
 
 import br.group.backendmarmitas.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,4 +20,6 @@ public class AuthorizationService  implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return repository.findByEmail(username);
     }
+
+
 }
