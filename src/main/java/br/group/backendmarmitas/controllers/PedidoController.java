6ca3cb885bd120d9143b74c21dff274432db41e1
TiagoMCFreitas <tiagofreitas85862@gmail.com>
2024-12-01 23:11:01 -0300

    arrumando pedidos

--- src/main/java/br/group/backendmarmitas/controllers/PedidoController.java
+++ src/main/java/br/group/backendmarmitas/controllers/PedidoController.java
@@ -30,10 +30,11 @@ public class PedidoController {
 
     @PostMapping("/")
     public ResponseEntity<PedidoDTO >insert(@Valid @RequestBody PedidoDTO dto){
-        System.out.println(dto);
+
         dto =  pedidoService.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
+
         return ResponseEntity.created(uri).body(dto);
     }
     @PutMapping (value = "/{id}")
--- src/main/java/br/group/backendmarmitas/services/DBservice.java
+++ src/main/java/br/group/backendmarmitas/services/DBservice.java
@@ -39,11 +39,11 @@ public class DBservice {
         userRepository.saveAll(Arrays.asList(user1, user2));
 
         // Seeding de Produtos
-        Produto produto1 = new Produto(null, "Marmita Tradicional", "Arroz, feijão, bife e salada", "https://example.com/marmita1.jpg", 12.50, 4, null);
-        Produto produto2 = new Produto(null, "Marmita Fitness", "Frango grelhado, batata doce e brócolis", "https://example.com/marmita2.jpg", 15.00, 5, null);
-        Produto produto3 = new Produto(null, "Marmita Vegana", "Quinoa, legumes grelhados e tofu", "https://example.com/marmita3.jpg", 14.00, 4, null);
-        Produto produto4 = new Produto(null, "Marmita Infantil", "Macarrão, nuggets e purê", "https://example.com/marmita4.jpg", 10.00, 3, null);
-        Produto produto5 = new Produto(null, "Marmita Premium", "Picanha, arroz à grega e farofa", "https://example.com/marmita5.jpg", 20.00, 5, null);
+        Produto produto1 = new Produto(null, "Marmita Tradicional", "Arroz, feijão, bife e salada", "https://example.com/marmita1.jpg", 12.50, 4);
+        Produto produto2 = new Produto(null, "Marmita Fitness", "Frango grelhado, batata doce e brócolis", "https://example.com/marmita2.jpg", 15.00, 5);
+        Produto produto3 = new Produto(null, "Marmita Vegana", "Quinoa, legumes grelhados e tofu", "https://example.com/marmita3.jpg", 14.00, 4);
+        Produto produto4 = new Produto(null, "Marmita Infantil", "Macarrão, nuggets e purê", "https://example.com/marmita4.jpg", 10.00, 3);
+        Produto produto5 = new Produto(null, "Marmita Premium", "Picanha, arroz à grega e farofa", "https://example.com/marmita5.jpg", 20.00, 5);
 
         produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3, produto4, produto5));
 
